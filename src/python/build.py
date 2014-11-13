d26fe6d680a3b15f5cba60ac40eb2d7006b75588
Steven Hoelscher <shoelsch@umich.edu>
2014-11-13 10:40:25 -0500

    Updated build code

--- src/python/build.py
+++ src/python/build.py
@@ -5,11 +5,13 @@ import csv
 import re
 from collections import Counter
 
+PRIVATE_PATH = "../../data/private/"
+PUBLIC_PATH = "../../data/public/"
 
 def buildAttendance():
     id_map = dict()
 
-    with open("../../data/private/withheld.csv", "rb") as inp:
+    with open(PRIVATE_PATH + "withheld.csv", "rb") as inp:
         cr = csv.reader(inp)
         cr.next()
         for row in cr:
@@ -19,10 +21,10 @@ def buildAttendance():
 
     seen = list()
 
-    with open("../../data/public/csv/attendance-2014.csv", "wb") as out:
+    with open(PUBLIC_PATH + "csv/attendance-2014.csv", "wb") as out:
         cw = csv.writer(out)
         cw.writerow(['ID', '6/24/2014', '6/25/2014', '6/26/2014', '6/27/2014', '7/1/2014', '7/2/2014', '7/3/2014', '7/8/2014', '7/9/2014', '7/10/2014', '7/11/2014', '7/15/2014', '7/16/2014', '7/17/2014', '7/18/2014', '7/22/2014', '7/23/2014', '7/24/2014', '7/25/2014', '7/29/2014', '7/30/2014', '7/31/2014', '8/1/2014', '8/5/2014', '8/6/2014', '8/7/2014', '8/8/2014', '8/12/2014', '8/13/2014', '8/14/2014', '8/15/2014'])
-        with open("../../data/private/attendance-2014.csv", "rb") as inp:
+        with open(PRIVATE_PATH + "attendance-2014.csv", "rb") as inp:
             cr = csv.reader(inp)
             for row in cr:
                 first, last = row[0].lower(), row[1].lower()
@@ -37,19 +39,18 @@ def buildAttendance():
 
 def buildVolunteers():
     SEEN = list()
-    PATH = "../../data/private/"
     FILES = ["volunteers-2014.csv", "volunteers-2013.csv"]
-
     uid = 0
 
-    with open("../../data/private/withheld.csv", "wb") as out_private:
+    with open(PRIVATE_PATH + "withheld.csv", "wb") as out_private:
         cw_private = csv.writer(out_private)
         cw_private.writerow(["ID", "FIRST", "LAST", "EMAIL", "PHONE"])
-        with open("../../data/public/csv/volunteers.csv", "w") as out_public:
+        with open(PUBLIC_PATH + "csv/volunteers.csv", "w") as out_public:
             cw_public = csv.writer(out_public)
-            cw_public.writerow(["ID", "DOB", "GENDER", "ADDRESS", "CITY", "STATE", "ZIPCODE", "ETHNICITY", "RELIGION", "HIGHSCHOOL", "GRAD_YEAR", "COLLEGE", "CARPOOL", "VOLUNTEERED", "VOLUNTEER_YEARS"])
+            cw_public.writerow(["ID", "YEAR", "DOB", "GENDER", "ADDRESS", "CITY", "STATE", "ZIPCODE", "ETHNICITY", "RELIGION", "HIGHSCHOOL", "GRAD_YEAR", "COLLEGE", "CARPOOL", "VOLUNTEERED", "VOLUNTEER_YEARS"])
             for f in FILES:
-                with open(PATH + f, "rU") as inp:
+                year = f.split("-")[1].split(".")[0]
+                with open(PRIVATE_PATH + f, "rU") as inp:
                     cr = csv.reader(inp)
                     cr.next()
                     for row in cr:
@@ -74,7 +75,7 @@ def buildVolunteers():
             
                             SEEN.append((first,last))
                             cw_private.writerow([uid, first, last, email, phone])
-                            cw_public.writerow([uid, dob, gender, address, city, state, zipcode, ethnicity, religion, highschool, gradyear, college, carpool, past, pastYears,])
+                            cw_public.writerow([uid, year, dob, gender, address, city, state, zipcode, ethnicity, religion, highschool, gradyear, college, carpool, past, pastYears,])
                             
                             uid += 1
 
